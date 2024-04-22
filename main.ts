8f6ff00655f0e06d07f4a2b49705771c8465b11c
Shehroz Hanif <shehrozhanif54@gmail.com>
2024-04-22 11:41:49 -0700

    Initial commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+const answers =await inquirer.prompt([
+    {
+        name: "Sentence",
+        type:"input",
+        message:chalk.yellow("Enter your sentence to check the words")
+
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+console.log(words)
+
+
+console.log(`${chalk.yellow("Words Counts")} = ${chalk.red(words.length)}`)
